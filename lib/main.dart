a853b048ef24a9e9f6c6f259fd2507fb09d7af83
afiqilyasakmal <afiq.ilyasa@ui.ac.id>
2022-11-17 14:17:52 +0700

    Update lab 08

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_lab_1/form.dart';
+import 'package:flutter_lab_1/page/form.dart';
+import 'package:flutter_lab_1/page/to_do_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -78,6 +79,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(builder: (context) => const MyFormPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('To Do'),
+              onTap: () {
+                // Route menu ke halaman to do
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ToDoPage()),
                 );
               },
             ),
